784f09cde881758f1931be785165c762165be22e
ppmyor <yorocobe@gmail.com>
2022-03-21 13:33:27 +0900

    chore: 일부 폰트 사이즈 수정

--- cat-jjal-maker-cra/src/components/Favorites.js
+++ cat-jjal-maker-cra/src/components/Favorites.js
@@ -3,7 +3,7 @@ import styles from "./Favorites.module.css";
 
 function Favorites({ favorites }) {
     if (favorites.length === 0) {
-        return <div>사진 위 하트를 눌러 고양이 사진을 저장해봐요!</div>;
+        return <span>사진 위 하트를 눌러 고양이 사진을 저장해봐요!</span>;
     }
 
     return (
